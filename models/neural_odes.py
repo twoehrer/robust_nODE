c3e3ded5ef6d3f6ca7314488ed858148c3a7814b
Tobias Woehrer <woehrer@am2-55-66.mi.uni-erlangen.de>
2021-10-20 16:00:11 +0200

    i need to turn off the grad for the adjoint trajectory as it is not needed

--- models/neural_odes.py
+++ models/neural_odes.py
@@ -385,7 +385,8 @@ class robNeuralODE(nn.Module):
         p2 = torch.tensor([0.,1.])
 
         #computes the solutions p(0) for the canonical initial conditions
-        self.adj_traj_p1 = self.adj_flow.trajectory(p1, self.time_steps)
+        self.adj_traj_p1 = self.adj_flow.trajectory(p1, self.time_steps).detach()
+        self.adj_traj_p1.requires_grad = False
         self.adj_traj_p2 = self.adj_flow.trajectory(p2, self.time_steps)
         
         if return_features:
